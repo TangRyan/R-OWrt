ca8124e74b432fba344a3eda9bfb64579d8d53f8
TangRyan <80312001+TangRyan@users.noreply.github.com>
2022-01-09 01:15:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-git clone https://github.com/coolsnowwolf/packages package/lean
+echo 'src-git lean https://github.com/coolsnowwolf/lede/tree/master/package/lean/ ' >>feeds.conf.default
